0f00ca654ead3e78be54131de52efe5d8b26e9f2
zzyzigy <zzyzigy@gmail.com>
2023-05-18 15:35:51 +0900

    DAOImpl 수정

--- src/main/java/zzyzzy/springboot/semiprojectv7/dao/JoinDAOImpl.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/dao/JoinDAOImpl.java
@@ -1,6 +1,5 @@
 package zzyzzy.springboot.semiprojectv7.dao;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import zzyzzy.springboot.semiprojectv7.model.Member;
