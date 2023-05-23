a666b66b444dbe9a2b3d7f711c6772b80773526e
zzyzigy <zzyzigy@gmail.com>
2023-05-23 12:20:12 +0900

    validation 2 - 컨트롤러에 get 요청시 유효성 검사할 모델 선언

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/BoardController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/BoardController.java
@@ -2,6 +2,7 @@ package zzyzzy.springboot.semiprojectv7.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,11 @@ public class BoardController {
     }
 
     @GetMapping("/write")
-    public String write() {
+    public String write(Model m) {
+
+        // validation을 위한 첫번째 코드
+        m.addAttribute("board", new Board());
+
         return "board/write";
     }
 
