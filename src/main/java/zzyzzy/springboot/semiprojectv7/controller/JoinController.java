b9c5ff9de2b91092d78f767aabe7dae950a9d082
zzyzigy <zzyzigy@gmail.com>
2023-05-23 18:12:58 +0900

    컨트롤러에 유효성 검사 모델 정의 및 검사 후 처리 구현 (회원정보)

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/JoinController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/JoinController.java
@@ -49,23 +49,32 @@ public class JoinController {
         return viewPage;
     }
 
-    @PostMapping("/joinme")
-    public ModelAndView joinme(Member mb) {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("join/joinme");
-        mv.addObject("mb", mb);
+    @GetMapping("/joinme")
+    public String joinme(Model m) {
 
-        return mv;
+        m.addAttribute("member", new Member());
+
+        return "join/joinme";
     }
 
-    @PostMapping("/joinok")
-    public String joinok(Member m, String grecaptcha) {
-        String view = "error";
+    @PostMapping("/joinme")
+    public String joinmeok(@Valid Member member,
+               BindingResult br, HttpSession sess) {
+        String viewPage = "redirect:/join/joinok";
 
-        if (jnsrv.newMember(m))
-            view = "join/joinok";
+        if (br.hasErrors()) viewPage = "join/joinme";
+        else {
+            jnsrv.newMember(member);
+            sess.invalidate();
+        }
 
-        return view;
+        return viewPage;
+    }
+
+    @GetMapping("/joinok")
+    public String joinok() {
+
+        return "join/joinok";
     }
 
     // 우편번호 검색
