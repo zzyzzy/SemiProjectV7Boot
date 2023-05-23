bf23c4f85c4a5c058cba942f6eebef5210d26281
zzyzigy <zzyzigy@gmail.com>
2023-05-23 18:11:44 +0900

    회원가입시 회원정보를 위한 모델 클래스 작성 (transient 애노테이션 추가)

--- src/main/java/zzyzzy/springboot/semiprojectv7/model/Member.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/model/Member.java
@@ -3,11 +3,12 @@ package zzyzzy.springboot.semiprojectv7.model;
 
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Table(name = "member")
@@ -25,16 +26,55 @@ public class Member {
     private String name;
     private String jumin1;
     private String jumin2;
+
+    @NotBlank(message = "아이디는 필수항목입니다!!")
+    @Length(min=6, max=16, message = "아이디는 최소6자, 최대16자이어야 합니다!!")
     private String userid;
+
+    @NotBlank(message = "비밀번호는 필수항목입니다!!")
+    @Length(min=6, max=16, message = "비밀번호는 최소6자, 최대16자이어야 합니다!!")
     private String passwd;
+
     private String zipcode;
+
+    @NotBlank(message = "기본주소는 필수항목입니다!!")
     private String addr1;
+
+    @NotBlank(message = "나머지주소는 필수항목입니다!!")
     private String addr2;
+
     private String email;
+
     private String phone;
 
     @CreatedDate
     @Column(insertable = false, updatable = false)
     private LocalDateTime regdate;
+
+    // -----------------------------
+
+    @Transient   // 필드 영속성은 지원 x
+    @NotBlank(message = "우편번호는 필수항목입니다!!")
+    private String zip1;
+
+    @Transient
+    @NotBlank(message = "우편번호는 필수항목입니다!!")
+    private String zip2;
+
+    @Transient
+    @NotBlank(message = "이메일은 필수항목입니다!!")
+    private String email1;
+
+    @Transient
+    @NotBlank(message = "이메일은 필수항목입니다!!")
+    private String email2;
+
+    @Transient
+    @NotBlank(message = "전화번호는 필수항목입니다!!")
+    private String tel2;
+
+    @Transient
+    @NotBlank(message = "전화번호는 필수항목입니다!!")
+    private String tel3;
 
 }
