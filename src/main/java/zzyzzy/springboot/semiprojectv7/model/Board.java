0dda23adbca49900d4c592d5f7a114ddfdf846d3
zzyzigy <zzyzigy@gmail.com>
2023-05-23 12:10:57 +0900

    validation 1 - 모델 클래스에 유효성 검사 어노테이션 추가

--- src/main/java/zzyzzy/springboot/semiprojectv7/model/Board.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/model/Board.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,7 +20,10 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
 
+    @NotBlank(message = "제목은 필수 입력항목입니다!!") // 문자열 길이 0, 공백 문자열 체크
     private String title;
+
+    @NotBlank(message = "작성자는 필수 입력항목입니다!!")
     private String userid;
 
     @Column(insertable = false, updatable = false)
@@ -28,6 +32,7 @@ public class Board {
     @Column(insertable = false, updatable = false)
     private Integer views;
 
+    @NotBlank(message = "본문은 필수 입력항목입니다!!")
     private String content;
 
     @CreatedDate
