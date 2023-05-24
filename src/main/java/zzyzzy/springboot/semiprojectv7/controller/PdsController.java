1cc41e880e01a2c13d527d95270c59999c5cf9b7
zzyzigy <zzyzigy@gmail.com>
2023-05-24 18:15:24 +0900

    자료실 - 컨트롤러 구현

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
@@ -1,16 +1,43 @@
 package zzyzzy.springboot.semiprojectv7.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import zzyzzy.springboot.semiprojectv7.model.Pds;
+import zzyzzy.springboot.semiprojectv7.service.PdsService;
 
 @Controller
 @RequestMapping("/pds")
 public class PdsController {
 
+    @Autowired
+    PdsService pdssrv;
+
     @GetMapping("/list")
     public String list() {
         return "pds/list";
+    }
+
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("pds", new Pds());
+
+        return "pds/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Pds pds, MultipartFile attach) {
+        String viewPage = "error";
+
+        int pno = pdssrv.newPds(pds);
+        if (pdssrv.newPdsAttach(attach, pno))
+            viewPage = "redirect:/pds/list";
+
+        return viewPage;
     }
 
 }
