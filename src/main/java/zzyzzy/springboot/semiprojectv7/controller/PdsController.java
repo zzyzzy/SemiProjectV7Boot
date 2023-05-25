0887089a117dba0daee5b10d0cfde25cf5a435d5
zzyzigy <zzyzigy@gmail.com>
2023-05-25 12:15:01 +0900

    자료실 - 본문보기 controller 구현

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
@@ -57,4 +57,13 @@ public class PdsController {
         return viewPage;
     }
 
+    @GetMapping("/view")
+    public String view(int pno, Model m) {
+
+        m.addAttribute("pds", pdssrv.readPds(pno));
+        m.addAttribute("attach", pdssrv.readPdsAttach(pno));
+
+        return "pds/view";
+    }
+
 }
