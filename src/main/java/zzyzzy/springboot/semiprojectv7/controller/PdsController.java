4c7b954dea47b9f157d9fd2403605dd62ea3af64
zzyzigy <zzyzigy@gmail.com>
2023-05-25 11:43:35 +0900

    자료실 - 목록보기 controller 구현

--- src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/controller/PdsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import zzyzzy.springboot.semiprojectv7.model.Pds;
 import zzyzzy.springboot.semiprojectv7.service.PdsService;
 
@@ -20,8 +21,19 @@ public class PdsController {
     PdsService pdssrv;
 
     @GetMapping("/list")
-    public String list() {
-        return "pds/list";
+    public ModelAndView list(Integer cpg) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pds/list");
+        if (cpg == null || cpg == 0) cpg = 1;
+
+        Map<String, Object> bds = pdssrv.readBoard(cpg);
+
+        mv.addObject("pdslist", bds.get("pdslist"));
+        mv.addObject("cpg", cpg);
+        mv.addObject("stpg", ((cpg - 1) / 10) * 10 + 1);
+        mv.addObject("cntpg", bds.get("cntpg"));
+
+        return mv;
     }
 
     @GetMapping("/write")
