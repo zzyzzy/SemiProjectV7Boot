ab0508e4c414aaf44ea713b8565172f85add719e
zzyzigy <zzyzigy@gmail.com>
2023-06-08 12:02:34 +0900

    갤러리 - gallery list projection

---
+++ src/main/java/zzyzzy/springboot/semiprojectv7/model/GalleryList.java
@@ -0,0 +1,8 @@
+package zzyzzy.springboot.semiprojectv7.model;
+
+//a.fname, g.gno, g.title, g.userid, g.regdate, g.thumbs, g.views
+
+public interface GalleryList {
+    String getFname();
+    Gallery getGallery();
+}
