820aa24c867ccb6c6e72ca05c6b1e4d15fa78417
zzyzigy <zzyzigy@gmail.com>
2023-06-08 11:53:15 +0900

    갤러리 - gallery list dao

--- src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAO.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAO.java
@@ -3,8 +3,12 @@ package zzyzzy.springboot.semiprojectv7.dao;
 import zzyzzy.springboot.semiprojectv7.model.GalAttach;
 import zzyzzy.springboot.semiprojectv7.model.Gallery;
 
+import java.util.Map;
+
 public interface GalleryDAO {
     int insertGal(Gallery gallery);
 
     int insertGalAttach(GalAttach ga);
+
+    Map<String, Object> selectGallery(Integer cpg);
 }
--- src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAOImpl.java
+++ src/main/java/zzyzzy/springboot/semiprojectv7/dao/GalleryDAOImpl.java
@@ -1,11 +1,17 @@
 package zzyzzy.springboot.semiprojectv7.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import zzyzzy.springboot.semiprojectv7.model.GalAttach;
 import zzyzzy.springboot.semiprojectv7.model.Gallery;
 import zzyzzy.springboot.semiprojectv7.repository.GalleryRepository;
 import zzyzzy.springboot.semiprojectv7.repository.GalleryaRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository("galdao")
 public class GalleryDAOImpl implements GalleryDAO {
@@ -28,6 +34,19 @@ public class GalleryDAOImpl implements GalleryDAO {
     @Override
     public int insertGalAttach(GalAttach ga) {
         return Math.toIntExact(galleryaRepository.save(ga).getGano());
+    }
+
+    @Override
+    public Map<String, Object> selectGallery(Integer cpg) {
+
+        Pageable paging = PageRequest.of(cpg, 25, Sort.Direction.DESC, "gno");
+
+        Map<String, Object> gals = new HashMap<>();
+        // Gallery와 GalAttach를 조인해서 리스트로 가져옴
+        gals.put("gallist", galleryRepository.findAllBy(paging).getContent());
+        gals.put("cntpg", galleryRepository.findAllBy(paging).getTotalPages());
+
+        return gals;
     }
 
 }
